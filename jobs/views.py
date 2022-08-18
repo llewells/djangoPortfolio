f12c2e1b1d0da6bbc18f1a6af293844300407c8d
Simon Llewellyn <llewells86@gmail.com>
2022-08-18 14:59:15 +0100

    Linked Postgres db to project and created example object

--- jobs/views.py
+++ jobs/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Job
 
 
 def home(request):
-    return render(request, 'jobs/home.html')
+    jobs = Job.objects # pulls in all the job objectes
+    return render(request, 'jobs/home.html', {'jobs':jobs})
